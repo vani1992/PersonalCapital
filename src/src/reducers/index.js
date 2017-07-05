ab94a532366886d49f28062a043d9a916d867b44
Vani <vanimerugu@gmail.com>
2017-07-04 20:50:14 -0700

    added styles

---
+++ src/src/reducers/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux';
+import InstitutesReducer from './reducer_institutes';
+
+const rootReducer = combineReducers({
+ 	institutes: InstitutesReducer
+});
+
+export default rootReducer;
